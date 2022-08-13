3e483d9a5bfeb753e60be45f02c0f6aa4ced6a23
karopeter <karopeter531@gmail.com>
2022-08-13 07:00:48 -0700

    calculator commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Switch  } from 'react-native';
+import { myColors } from './src/styles/Colors';
+import { ThemeContext } from './src/contexts/ThemeContext';
+import MyKeyboard from './src/components/mykeyboard';
 
 export default function App() {
+  const [theme, setTheme] = useState();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContext.Provider value={theme}>
+        <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: 'black' }]}>
+           <Switch 
+             value={theme}
+             onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+           />
+           <MyKeyboard />
+        </SafeAreaView>
+    </ThemeContext.Provider>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: myColors.light,
+    justifyContent: 'flex-start',
+  }
 });
